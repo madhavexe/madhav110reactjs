940b37f3c3159d4caa18e9efb445963b7c9c2aaf
madhav <madhavjoshi6280@gmail.com>
2026-07-27 16:30:42 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {data} from '../data'
-
+import {Link} from 'react-router-dom'
 const Navbar = () => {
     const {name, role} = data
     return (
         <nav className='flex justify-between items-center px-7 py-4 border-b border-b-1'>
+            <Link to='/'>
             <span className='text-2xl font-semibold'>{name}</span>
+            </Link>
             <ul className='flex gap-5 items-center'>
                 <li>About</li>
                 <li>Contact</li>
