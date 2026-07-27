58d91bc01ee12e18611d53b4ecd79318fa0f8709
madhav <madhavjoshi6280@gmail.com>
2026-07-27 16:27:20 +0530

    9th push

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,7 +4,7 @@ const ProjectCard = ({title, description }) => {
   return (
     <div className='flex flex-col items-center justify-center gap-4 border rounded-xl shadow-sm bg-blue-100 py-3 px-5 w-[50%]'>
 <h3 className='text-xl font-semibold'>{title}</h3>
-<p className='text-gray-600'>{description}</p>
+<p className='text-gray-600 text-center'>{description}</p>
     </div>
   )
 }
