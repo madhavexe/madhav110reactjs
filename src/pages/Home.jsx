2bda620b78ca377057a9512f567e699bced6f94f
madhav <madhavjoshi6280@gmail.com>
2026-07-27 15:03:55 +0530

    3rd push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,9 @@ const Home = () => {
   return (
     <div className='flex flex-col gap-10 items-center justify-center md:h-screen'>
         <h1 className='text-xl md:text-2xl'>Welcome to my Portfolio</h1>
-       <div className='flex flex-col gap-6 items-center justify-center w-[70%]'>
-         <p>Hello, I'm <span className='text-xl'>{role}</span>, a {role}.</p>
+       <div className='flex flex-col gap-6 text-center items-center justify-center md:w-[70%] shadow-sm bg-amber-100 rounded-xl p-5 md:py-15 md:px-6'>
+        <img src="https://cdn-icons-png.flaticon.com/512/6714/6714029.png" className='w-[150px]' alt="" />
+         <p>Hello, I'm <span className='text-xl'>{name}</span>, a <span className='text-xl'>{role}</span>.</p>
         <p>{bio}</p>
         <small>Tags : {tags}</small>
        </div>
