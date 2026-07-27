3c4e4c26b2dd9c4bb867adc2949c49bcee0f9813
madhav <madhavjoshi6280@gmail.com>
2026-07-27 15:10:38 +0530

    4th push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,13 @@ const Home = () => {
     const {name, role, bio, tags} = data
   return (
     <div className='flex flex-col gap-10 items-center justify-center md:h-screen'>
-        <h1 className='text-xl md:text-2xl'>Welcome to my Portfolio</h1>
+        <h1 className='text-xl md:text-3xl'>Welcome to my Portfolio</h1>
        <div className='flex flex-col gap-6 text-center items-center justify-center md:w-[70%] shadow-sm bg-amber-100 rounded-xl p-5 md:py-15 md:px-6'>
-        <img src="https://cdn-icons-png.flaticon.com/512/6714/6714029.png" className='w-[150px]' alt="" />
+        <img src="https://cdn-icons-png.flaticon.com/512/6714/6714029.png" className='w-[150px] hover:scale-105 transition' alt="" />
          <p>Hello, I'm <span className='text-xl'>{name}</span>, a <span className='text-xl'>{role}</span>.</p>
         <p>{bio}</p>
         <small>Tags : {tags}</small>
+        <button className='btn'>Know More</button>
        </div>
     </div>
   )
